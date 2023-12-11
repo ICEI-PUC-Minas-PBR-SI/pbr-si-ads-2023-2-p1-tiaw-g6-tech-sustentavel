860f3c24a6a4988450d9f1092f88f5fdb9bfce39
CarlosGSC7 <carlosgsc87@gmail.com>
2023-12-11 11:22:52 -0300

    retorno

--- src/Js/produtos.js
+++ src/Js/produtos.js
@@ -2,7 +2,7 @@ function carregarProdutos() {
     const urlParams = new URLSearchParams(window.location.search)
     const pesquisa = urlParams.get("s")
     const urlServico = pesquisa ? `&q=${pesquisa}` : "";
-    fetch(`https://pbr-si-ads-2023-2-p1-tiaw-g6-tech-sustentavel-stw1.vercel.app/produtos`)
+    fetch(`http://localhost:5000/produtos?finalizado=false${urlServico}`)
         .then(response => response.json())
         .then(produtos => {
             if (produtos.length == 0) {
